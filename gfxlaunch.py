ca496c48cc5d9db8f43e81684a23cea577f26d6e
Jonas Lindemann <jonas.lindemann@lunarc.lu.se>
2023-06-15 14:00:17 +0200

    Cleaned up start script. Changed Exclusive to Full Node and updated the job options to an icon.

--- gfxlaunch.py
+++ gfxlaunch.py
@@ -33,82 +33,12 @@ This is free software, and you are welcome to redistribute it
 under certain conditions; see LICENSE for details.
 """
 gfxlaunch_copyright_short = """LUNARC HPC Desktop On-Demand - %s"""
-gfxlaunch_version = "0.9.5a"
+gfxlaunch_version = "0.9.6"
 
 # --- Fix search path for tool
 
 tool_path = os.path.dirname(os.path.abspath(sys.argv[0]))
 sys.path.insert(0, tool_path)
-# sys.path.append(tool_path)
-
-
-def create_dark_palette():
-    """Create a dark palette"""
-    darkPalette = QtGui.QPalette()
-    darkPalette.setColor(QtGui.QPalette.Window, QtGui.QColor(53, 53, 53))
-    darkPalette.setColor(QtGui.QPalette.WindowText, QtCore.Qt.white)
-    darkPalette.setColor(QtGui.QPalette.Disabled,
-                         QtGui.QPalette.WindowText, QtGui.QColor(127, 127, 127))
-    darkPalette.setColor(QtGui.QPalette.Base, QtGui.QColor(42, 42, 42))
-    darkPalette.setColor(QtGui.QPalette.AlternateBase,
-                         QtGui.QColor(66, 66, 66))
-    darkPalette.setColor(QtGui.QPalette.ToolTipBase, QtCore.Qt.white)
-    darkPalette.setColor(QtGui.QPalette.ToolTipText, QtCore.Qt.white)
-    darkPalette.setColor(QtGui.QPalette.Text, QtCore.Qt.white)
-    darkPalette.setColor(QtGui.QPalette.Disabled,
-                         QtGui.QPalette.Text, QtGui.QColor(127, 127, 127))
-    darkPalette.setColor(QtGui.QPalette.Dark, QtGui.QColor(35, 35, 35))
-    darkPalette.setColor(QtGui.QPalette.Shadow, QtGui.QColor(20, 20, 20))
-    darkPalette.setColor(QtGui.QPalette.Button, QtGui.QColor(53, 53, 53))
-    darkPalette.setColor(QtGui.QPalette.ButtonText, QtCore.Qt.white)
-    darkPalette.setColor(QtGui.QPalette.Disabled,
-                         QtGui.QPalette.ButtonText, QtGui.QColor(127, 127, 127))
-    darkPalette.setColor(QtGui.QPalette.BrightText, QtCore.Qt.red)
-    darkPalette.setColor(QtGui.QPalette.Link, QtGui.QColor(42, 130, 218))
-    darkPalette.setColor(QtGui.QPalette.Highlight, QtGui.QColor(42, 130, 218))
-    darkPalette.setColor(QtGui.QPalette.Disabled,
-                         QtGui.QPalette.Highlight, QtGui.QColor(80, 80, 80))
-    darkPalette.setColor(QtGui.QPalette.HighlightedText, QtCore.Qt.white)
-    darkPalette.setColor(QtGui.QPalette.Disabled,
-                         QtGui.QPalette.HighlightedText, QtGui.QColor(127, 127, 127))
-
-    return darkPalette
-
-
-def create_light_palette():
-    """Create light palette"""
-
-    lightPalette = QtGui.QPalette()
-
-    lightPalette.setColor(QtGui.QPalette.Window, QtGui.QColor(212, 212, 212))
-    lightPalette.setColor(QtGui.QPalette.WindowText, QtCore.Qt.black)
-    lightPalette.setColor(QtGui.QPalette.Disabled,
-                          QtGui.QPalette.WindowText, QtGui.QColor(172, 172, 172))
-    lightPalette.setColor(QtGui.QPalette.Base, QtGui.QColor(255, 255, 230))
-    lightPalette.setColor(QtGui.QPalette.AlternateBase,
-                          QtGui.QColor(0, 255, 0))
-    lightPalette.setColor(QtGui.QPalette.ToolTipBase,
-                          QtGui.QColor(255, 255, 230))
-    lightPalette.setColor(QtGui.QPalette.ToolTipText, QtCore.Qt.black)
-    lightPalette.setColor(QtGui.QPalette.Text, QtCore.Qt.black)
-    lightPalette.setColor(QtGui.QPalette.Disabled,
-                          QtGui.QPalette.Text, QtGui.QColor(128, 128, 128))
-    lightPalette.setColor(QtGui.QPalette.Dark, QtGui.QColor(160, 160, 160))
-    lightPalette.setColor(QtGui.QPalette.Shadow, QtGui.QColor(100, 100, 100))
-    lightPalette.setColor(QtGui.QPalette.Button, QtGui.QColor(212, 212, 212))
-    lightPalette.setColor(QtGui.QPalette.ButtonText, QtCore.Qt.black)
-    lightPalette.setColor(QtGui.QPalette.Disabled,
-                          QtGui.QPalette.ButtonText, QtGui.QColor(172, 172, 172))
-    lightPalette.setColor(QtGui.QPalette.BrightText, QtCore.Qt.red)
-    lightPalette.setColor(QtGui.QPalette.Link, QtGui.QColor(42, 130, 218))
-    lightPalette.setColor(QtGui.QPalette.Highlight, QtGui.QColor(42, 130, 218))
-    lightPalette.setColor(QtGui.QPalette.Disabled,
-                          QtGui.QPalette.Highlight, QtGui.QColor(80, 80, 80))
-    lightPalette.setColor(QtGui.QPalette.HighlightedText, QtCore.Qt.white)
-    lightPalette.setColor(QtGui.QPalette.Disabled,
-                          QtGui.QPalette.HighlightedText, QtGui.QColor(127, 127, 127))
-
-    return lightPalette
 
 
 if __name__ == '__main__':
@@ -224,13 +154,9 @@ if __name__ == '__main__':
     launch_settings.copyright_short_info = gfxlaunch_copyright_short
     launch_settings.version_info = gfxlaunch_version
 
-    # User interface
-
-    dark_mode = False
-
     # Redirect standard output
 
-    redirect = True
+    redirect = False
 
     # Create Queue and redirect sys.stdout to this queue
 
@@ -243,13 +169,6 @@ if __name__ == '__main__':
     # Create application object
 
     app = QtWidgets.QApplication(sys.argv)
-
-    #if dark_mode:
-    #    palette = create_dark_palette()
-    #else:
-    #    palette = create_light_palette()
-
-    #app.setPalette(palette)
 
     # Show splash
 
@@ -278,7 +197,5 @@ if __name__ == '__main__':
         form.redirect_thread = thread
 
     # Start main application loop
-
-    # app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
 
     app.exec_()
